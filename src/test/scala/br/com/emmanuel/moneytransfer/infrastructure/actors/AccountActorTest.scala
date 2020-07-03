0dbe2550f19ea5d12b90a52a5c94b69b3efe6470
Emmanuel Silva <emmanuel.silva@gmail.com>
2020-07-03 16:07:00 -0300

    Add more than one deposit test scenario

--- src/test/scala/br/com/emmanuel/moneytransfer/infrastructure/actors/AccountActorTest.scala
+++ src/test/scala/br/com/emmanuel/moneytransfer/infrastructure/actors/AccountActorTest.scala
@@ -1,6 +1,7 @@
 package br.com.emmanuel.moneytransfer.infrastructure.actors
 
-import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe}
+import akka.actor.typed.ActorRef
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class AccountActorTest extends WordSpecLike with BeforeAndAfterAll {
@@ -11,24 +12,55 @@ class AccountActorTest extends WordSpecLike with BeforeAndAfterAll {
 
   "should receive deposit and change balance amount" in {
     val accountId = "123"
-    val accountActor = testKit.spawn(AccountActor(accountId))
+    val accountActor = createAccountActor(accountId)
 
     accountActor ! AccountActor.Deposit(100)
 
-    val probe = testKit.createTestProbe[AccountActor.Response]()
+    val probe = createAccountResponseProbe
     accountActor ! AccountActor.GetBalance(probe.ref)
 
-    probe.expectMessage(AccountActor.Balance(accountId, 100))
+    assertThatExpectedBalanceIs(probe, accountId, 100)
+  }
+
+  "should receive all deposits message and change balance" in {
+    val accountId = "123"
+    val accountActor = createAccountActor(accountId)
+    val depositAmount = 100
+    val depositQuantity = 1000
+
+    for (_ <- 1 to depositQuantity) {
+      accountActor ! AccountActor.Deposit(depositAmount)
+    }
+
+    val probe = createAccountResponseProbe
+    accountActor ! AccountActor.GetBalance(probe.ref)
+
+    assertThatExpectedBalanceIs(probe, accountId, depositAmount * depositQuantity)
   }
 
   "should retrieve balance amount" in {
     val accountId = "123"
-    val accountActor = testKit.spawn(AccountActor(accountId))
+    val accountActor = createAccountActor(accountId)
 
-    val probe = testKit.createTestProbe[AccountActor.Response]()
+    val probe = createAccountResponseProbe
     accountActor ! AccountActor.GetBalance(probe.ref)
 
-    probe.expectMessage(AccountActor.Balance(accountId, 0))
+    assertThatExpectedBalanceIs(probe, accountId, 0)
   }
+
+  private def createAccountActor(accountId: String) =
+    testKit.spawn(AccountActor(accountId))
+
+
+  private def createAccountResponseProbe =
+    testKit.createTestProbe[AccountActor.Response]()
+
+
+  private def assertThatExpectedBalanceIs(probe: TestProbe[AccountActor.Response],
+                                          accountId: String,
+                                          amount: BigDecimal): Unit = {
+    probe.expectMessage(AccountActor.Balance(accountId, amount))
+  }
+
 
 }
