9a2af991cacbf701a8d81a69f75c97c14e7a3dd6
Emmanuel Silva <emmanuel.silva@gmail.com>
2020-12-08 04:18:27 +0100

    Refactor - Remove unnecessary parentheses

--- src/main/scala/br/com/emmanuel/moneytransfer/domain/Transaction.scala
+++ src/main/scala/br/com/emmanuel/moneytransfer/domain/Transaction.scala
@@ -2,6 +2,6 @@ package br.com.emmanuel.moneytransfer.domain
 
 sealed trait Transaction
 
-case class DepositTransaction(amount: BigDecimal) extends Transaction()
+case class DepositTransaction(amount: BigDecimal) extends Transaction
 
-case class WithdrawTransaction(amount: BigDecimal) extends Transaction()
+case class WithdrawTransaction(amount: BigDecimal) extends Transaction
